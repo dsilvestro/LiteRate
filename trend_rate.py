aba3467e1d6fb68e4138a41f5c7032a4f4d4212b
Bernard  J Koch <akathriel@gmail.com>
2019-05-19 12:20:57 -0700

    Trend Rate Model Update
    Not yet working but close

--- trend_rate.py
+++ trend_rate.py
@@ -27,89 +27,102 @@ print("\n\n             TrendRate - 20190205\n")
 ####SET UP####
 
 p = core_arguments()
-p.add_argument('-corr_B', type=int, help="set to: 1 for linear, 2 for exp" , default=1, metavar=1)
-p.add_argument('-corr_D', type=int, help="set to: 1 for linear, 2 for exp" , default=1, metavar=1)
 #ADD ANY EXTRA ARGUMENTS YOU NEED AFTER
 
 #parse arguments for globals
+p.add_argument('trend_data', metavar='<path to trend file>', type=str,help='Input trend file should be columns tab-separated with headers. No missing values.',default="")
+p.add_argument('trend_index', type=int,help='Column of trend in trend file.',default=0,metavar=0)
+p.add_argument('-const_B', type=bool, help='F) Vary rates with trend T) Constant rates', default=False,metavar=False)
+p.add_argument('-const_D', type=bool, help='F) Vary rates with trend T) Constant rates', default=False,metavar=False)
+
 args = p.parse_args()
+
 CONST_BIRTH=args.const_B
 CONST_DEATH=args.const_D
-CORR_B=args.corr_B
-CORR_D=args.corr_D
-if CONST_BIRTH==TRUE: CORR_B=0
-if CONST_DEATH==TRUE: CORR_D=0
 
 set_seed(args.seed)
 
-TS,TE,PRESENT,ORIGIN=parse_ts_te(args.d,args.first_year,args.last_year,args.death_jitter)
+TS,TE,PRESENT,ORIGIN=parse_ts_te(args.d,args.TBP,args.first_year,args.last_year,args.death_jitter)
 
-N_SPEC, N_EXTI, DT, N_TIME_BINS, TIME_RANGE=create_bins(ORIGIN, PRESENT,TS,TE,args.rm_first_bin)
+ORIGIN, PRESENT, N_SPEC, N_EXTI, DT, N_TIME_BINS, TIME_RANGE=create_bins(ORIGIN, PRESENT,TS,TE,args.rm_first_bin)
+
+B_EMP,D_EMP=print_empirical_rates(N_SPEC,N_EXTI,DT)
+
+def parse_trend_data(trend_file_path, index):
+	trend_matrix=t_file=np.loadtxt(trend_file_path, skiprows=1)
+	trend=trend_matrix[:,index]
+	max_t=np.max(trend)
+	min_t=np.min(trend)
+	trend=(trend-max_t)/(max_t-min_t)
+	return trend
+
+TREND=parse_trend_data(args.trend_file_path,args.index)
 
 def likelihood_function(args):
-	[l_min,m_min,l_corr,m_corr] = args
+	[l_min, m_min, alpha, beta,delta,gamma]= args
 	if CONST_BIRTH==True:
 		birth_rates = np.ones(n_time_bins)*l_min
 #		birth_lik = np.sum(n_spec*log(birth_rates)-birth_rates*np.sum(Dt)) # log probability of speciation
-	elif CORR_B==1:
-		# lik speciation
-		birth_rates = l_min + l_min * l_corr * TREND
-	elif CORR_B==2:
-		birth_rates = l_min + exp(l_corr * TREND)
+	else:
+		birth_rates = l_min + (l_min * TREND) ** delta
+
 	birth_lik = np.sum(log(birth_rates)*N_SPEC - birth_rates*DT)
 
 	if CONST_DEATH==True:	
 		death_rates = np.ones(n_time_bins) *m_min
-	elif CORR_B ==1:
-		death_rates =  m_min + m_min * m_corr * TREND
-	elif CORR_B ==2:
-		death_rates = m_min + exp(m_corr * TREND)
+    else:
+		death_rates =  m_min +  (m_min * TREND) ** gamma
 	death_lik = np.sum(log(death_rates)*N_EXTI - death_rates*DT)
 
 	lik = np.array([birth_lik, death_lik])
 	return [lik, birth_rates, death_rates]
 
 def calc_priors(args):
-	p = prior_gamma(args[0],a=1,s=10,l=0) #l_min
-	p += prior_gamma(args[1],a=1,s=10,l=0) #m_min
-	p += prior_norm(args[2],loc=0,scale=1) #l corr
-	p += prior_norm(args[2],loc=0,scale=1) #m corr
+	p = prior_gamma(args[0],a=1,s=10,l.=001) #l_min
+	p += prior_gamma(args[1],a=1,s=10,l=.001) #m_min
+    	#p += prior_gamma(args[2],a=1,s=10,l=0) #alpha
+    	#p += prior_gamma(args[3],a=1,s=10,l=0) #beta
+	p += prior_norm(args[4],loc=0,scale=1) #delta
+	p += prior_norm(args[5],loc=0,scale=1) #gamma
 	
 
 def __main__(parsed_args):
 	out=""
 	if CONST_BIRTH:out+="_CONB"
-	if CONST_DEATH:out+="_COND"
-	if CORR_B==1:out+="_LINB"
 	else:out+="_EXPB"
-	if CORR_D==1:out+="_LIND"
+	if CONST_DEATH:out+="_COND"
 	else: out+="_EXPD"
 	
 	outfile = "%s_%s%s.log" % (os.path.splitext(args.d)[0], seed, out)
 	logfile = open(outfile , "w") 
 	wlog=csv.writer(logfile, delimiter='\t')
-	head =["it","posterior","likelihood","likelihood_birth","likelihood_death","prior","l_min","m_min","l_corr","m_corr"]
+	head =["it","posterior","likelihood","likelihood_birth","likelihood_death","prior","l_min","m_min","alpha","beta","delta","gamma"]
 	for i in range(len(Dt)): head.append("l_%s" % i)
 	for i in range(len(Dt)): head.append("m_%s" % i)
+	head+=["corr_coeff","rsquared","gelman_r2"]
+
 	wlog.writerow(head)
 
 
 	#SETUP PROPOSALS
 							
-	update_multiplier = np.array([1,  1]) #l_min, m_min								
-	update_sliding_window = np.array([0,  0]) #corr_l, corr_m
-	if CONST_BIRTH==False:		update_sliding_window[0]=1
-	if CONST_DEATH==False:		update_sliding_window[1]=1
+	update_multiplier = np.array([1,  1, 1, 1]) #l_min, m_min, delta, gamma								
+	if CONST_BIRTH==False:	
+		update_multiplier[2]=1
+	if CONST_DEATH==False:	
+		update_sliding_window[1]=1
 	
 	update_multiplier = update_multiplier/sum(update_multiplier)
-	update_sliding_window = update_sliding_window/sum(update_sliding_window)
+	#update_sliding_window = update_sliding_window/sum(update_sliding_window)
 
 	###INITIAL VALUES###
 	l_min=.001
 	m_min=.001
-	corr_l=0
-	corr_m=0
-	argsA=np.array([l_min, m_min, l_corr, m_corr])
+	alpha=.001
+	beta=.001
+	delta=0
+	gamma=0
+	argsA=np.array([l_min, m_min, alpha, beta,delta,gamma])
 	lik_res = likelihood_function(argsA)
 	likA = np.sum(lik_res[0])
 	likBirthA = lik_res[0][0]
@@ -126,13 +139,9 @@ def __main__(parsed_args):
 		args = argsA+0.
 		updated_ext = 0
 		hastings= 0
-		rr = np.random.random()
-		if rr[1]<0.5 and CONST_K==0:
-			res = update_slidwin_proposal_vec(res, m=-1, M=1, d=1.,f=update_sliding_window) #update midpoint (the only sliding window proposal)
-		else:
-			res = update_multiplier_proposal_vec(args,d=1.1,f=update_multiplier) #update everything with multipliers	
-		[args, hastings] = res
-		lik_res = likelihood_function(args,)
+		rr = np.random.random(2)
+		[args, hastings]=update_multiplier_proposal_vec(args,d=1.1,f=update_multiplier) #update everything with multipliers	
+		lik_res = likelihood_function(args)
 		lik = np.sum(lik_res[0])
 		prior = calc_prior(args)
 		if (lik - likA) + (prior - priorA) + hastings > log(np.random.random()) or iteration==0:
@@ -143,13 +152,13 @@ def __main__(parsed_args):
 			likDeathA = lik_res[0][1]
 			birth_rates = lik_res[1]
 			death_rates = lik_res[2]
-			niche = lik_res[3]
-			nicheFrac = lik_res[4]
+
 		if iteration % sampling_freq==0:
 			#print lik,prior, args
 			argsO=deepcopy(argsA) #when you copy lists, makes sure you dont change things by reference
 			print(iteration, likA, argsO) #, args
-			l= [iteration,likA+priorA, likA,likBirthA,likDeathA, priorA] + list(argsO) + list(birth_rates) + list(death_rates)
+			adequacy=calculate_r_squared(B_EMP,D_EMP,birth_rates,death_rates)
+			l= [iteration,likA+priorA, likA,likBirthA,likDeathA, priorA] + list(argsO) + list(birth_rates) + list(death_rates) + list(adequacy)
 			wlog.writerow(l)
 			logfile.flush()
 			os.fsync(logfile)
